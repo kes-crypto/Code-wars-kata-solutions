383819acfdd13c40535863fdab0d5fbd16b32d64
Kestone Dennis <denniskestone@gmail.com>
2023-08-02 17:46:57 +0300

    Update encodeDecode.js

--- encodeDecode.js
+++ encodeDecode.js
@@ -9,3 +9,9 @@ const
 console.log(encode('This is an encoding test.'))
 encode('How are you today?')
 encode('This is an encoding test.')
+
+const vowels = ['a', 'e' ,'i', 'o', 'u'];
+const v4w2ls = ['1', '2', '3', '4', '5']; 
+
+const encode = str => [...str].map(e => vowels.includes(e) ? e = vowels.indexOf(e) + 1 : e).join('');
+const decode = str => [...str].map(e => v4w2ls.includes(e) ? e = vowels[v4w2ls.indexOf(e)] : e).join('');
