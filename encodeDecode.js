528a1f191d87d2ebef6222a4116310dd4affabc4
Kestone Dennis <denniskestone@gmail.com>
2023-08-02 17:48:16 +0300

    Update encodeDecode.js

--- encodeDecode.js
+++ encodeDecode.js
@@ -1,3 +1,4 @@
+sol 1
 const
     codeFunctionForMapping = replacementMapping => (
       inputString => inputString.replace(/./g, c => replacementMapping[c] || c)
@@ -10,8 +11,16 @@ console.log(encode('This is an encoding test.'))
 encode('How are you today?')
 encode('This is an encoding test.')
 
+//sol 2
 const vowels = ['a', 'e' ,'i', 'o', 'u'];
 const v4w2ls = ['1', '2', '3', '4', '5']; 
 
 const encode = str => [...str].map(e => vowels.includes(e) ? e = vowels.indexOf(e) + 1 : e).join('');
 const decode = str => [...str].map(e => v4w2ls.includes(e) ? e = vowels[v4w2ls.indexOf(e)] : e).join('');
+
+//sol 3
+(def e {\a 1 \e 2 \i 3 \o 4 \u 5})
+(def d [\a\e\i\o\u])
+
+(defn encode [s] (apply str (map #(get e % %) s)))
+(defn decode [s] (apply str (map #(get d (- (int %) (int \1)) %) s)))
