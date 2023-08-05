a930bb503ebfeb5c4e0b90bb596ed8b88dc9dcb9
Kestone Dennis <denniskestone@gmail.com>
2023-08-05 15:14:24 +0300

    Update Calculating.js

--- Calculating.js
+++ Calculating.js
@@ -15,5 +15,19 @@ Division should be integer division. For example, this should return 2, not 2.66
 eight(dividedBy(three()));
 
 */
+function zero() {}
+function one() {}
+function two() {}
+function three() {}
+function four() {}
+function five() {}
+function six() {}
+function seven() {}
+function eight() {}
+function nine() {}
 
+function plus() {}
+function minus() {}
+function times() {}
+function dividedBy() {}
 
