d8be00a69ed8e1b4a3551688d63a2f8c03a133a7
Kestone Dennis <denniskestone@gmail.com>
2023-08-09 12:49:46 +0300

    Update addNumberOnArray.js

--- addNumberOnArray.js
+++ addNumberOnArray.js
@@ -1,1 +1,15 @@
+function addNumbers(n)
+{
+  var foo = [];
 
+for (var i = 0; i <= n; i++) {
+   foo.push(i);
+}
+let sum = 0;
+for (var i = 0; i < foo.length; i++) {
+        sum += foo[i]
+    }
+return sum;
+}
+console.log(addNumbers(6))
+//output 21
