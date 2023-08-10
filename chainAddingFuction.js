68bfc66fa749d2edab37679e20e3029356dcd991
Kestone Dennis <denniskestone@gmail.com>
2023-08-10 14:26:43 +0300

    Update chainAddingFuction.js

--- chainAddingFuction.js
+++ chainAddingFuction.js
@@ -18,4 +18,42 @@ var add = function(n) {
   return f;
 }
 //sol 3
+function add(n){
+  var a = function(x){ return add(x + n) }
+  a.valueOf = function(){ return n }
+  return a
+}
+//sol 4
+function add(n){
+  var fn = function(x) {
+    return add(n + x);
+  };
+  
+  fn.valueOf = function() {
+    return n;
+  };
+  
+  return fn;
+}
+//sol 5
+function add(n){
+  var fn = function(x){ return add(n+x); }
+  fn.toString = function(){ return n; }
+  return fn;
+}
+//sol 6
+function add(n){
+  
+  var sum = n;
+  
+  var f = function(n) {
+    sum += n;
+    return f;
+  };
+  
+  f.valueOf = function() {
+    return sum;
+  }
 
+  return f;
+}
