5c8833bf07a236742de7f7b44a67172382f6eaf4
Dennis Kestone <denniskestone@gmail.com>
2023-08-14 18:05:29 +0300

    Update covertToBinary.js

--- covertToBinary.js
+++ covertToBinary.js
@@ -25,3 +25,13 @@ function decimalToBinary(decimal){
 function addBinary(a,b) {
   return decimalToBinary(a+b);
 }
+//sol 4
+function addBinary(a,b) {
+  var c = a + b;
+  var res = '';
+  while (c >= 1) {
+    var res = c % 2 + res;
+    c = Math.floor(c / 2);
+  }
+  return res;
+}
