6969c29221a6346cf058dd184227c7413af79fcc
Dennis Kestone <denniskestone@gmail.com>
2023-08-15 10:42:49 +0300

    Update sumOfNumbers.js

--- sumOfNumbers.js
+++ sumOfNumbers.js
@@ -30,5 +30,26 @@ function getSum(a, b) {
    else if (a < b) return a + GetSum(a+1, b);
    else return a + GetSum(a-1,b);
 }
+//sol 3
+function GetSum( a,b )
+{
+//if both a and b are equal return a
+   if(a===b){
+     return a;
+   }
+   var result = 0;
+   
+   var x = a, y = b;
+ // check the larger and smaller numbers and assign them to x and y
+   if(a > b) {
+      x = b, y = a;
+   }
+ //in a loop add the numbers from the smaller one until it reaches the larger number
+   for(var i = x; i <=y; i++ ) {
+     result += i;
+   }
+   return result;
+   
+}
   
 
