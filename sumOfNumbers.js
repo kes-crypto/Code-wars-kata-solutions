7c82d022bfa3ce833fea6a0d84909bb4ec318bd3
Dennis Kestone <denniskestone@gmail.com>
2023-08-15 10:40:50 +0300

    Update sumOfNumbers.js

--- sumOfNumbers.js
+++ sumOfNumbers.js
@@ -21,4 +21,13 @@ function getSum(a, b) {
     sum += i;
   }
   return sum;
+
+  //sol 2
+  function GetSum( a,b )
+{
+   if (a == b) return a;
+   else if (a < b) return a + GetSum(a+1, b);
+   else return a + GetSum(a-1,b);
+}
+  
 }
