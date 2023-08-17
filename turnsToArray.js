593637854086877568d9f89ff7baa93279695af8
Dennis Kestone <denniskestone@gmail.com>
2023-08-17 11:07:01 +0300

    Update turnsToArray.js

--- turnsToArray.js
+++ turnsToArray.js
@@ -3,3 +3,13 @@ function squareDigits(num){
   console.log(arrayOfDigits)
 }
 squareDigits(23452637)
+//explanation
+function squareDigits(num){
+  //turning the number to an array
+  const arrayOfDigits = num.toString().split('')
+  //squaring each digit of the created array
+  const map1 = arrayOfDigits.map((x) => x * x);
+  //joining back the result in number format
+  const map = Number(map1.join(""))
+  return map
+}
