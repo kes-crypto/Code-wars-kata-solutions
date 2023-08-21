6a52d4238c674e0b06dd4c3c46a203ae2110ee30
Dennis Kestone <denniskestone@gmail.com>
2023-08-21 17:59:44 +0300

    Update betterThanAverage.js

--- betterThanAverage.js
+++ betterThanAverage.js
@@ -1,1 +1,19 @@
+function betterThanAverage(classPoints, yourPoints) {
+var items = classPoints.length
+  // iterate over each item in the array
+  let sum = 0
+for (let i = 0; i < items; i++ ) {
+  
+  sum += classPoints[i];
+}
+var average = sum/items
+if(average > yourPoints){
+  return true
+}
+if(average < yourPoints){
+  return false
+}
+ 
+}
+console.log(betterThanAverage([41, 75, 72, 56, 80, 82, 81, 33], 45))
 
