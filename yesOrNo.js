27ea6ea94507be71cdbbef0f5f4d373fb666d302
Dennis Kestone <denniskestone@gmail.com>
2023-08-22 17:43:16 +0300

    Update yesOrNo.js

--- yesOrNo.js
+++ yesOrNo.js
@@ -1,4 +1,7 @@
-
+/*
+DESCRIPTION:
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+*/
 //sol1
 function boolToWord( bool ){
   return bool ? 'Yes':'No';
