25f0bd94780cdd9751f58bff76aa2b67d59fb997
Dennis Kestone <denniskestone@gmail.com>
2023-08-23 10:02:16 +0300

    Update name.js

--- name.js
+++ name.js
@@ -1,3 +1,8 @@
 function greet(name){
   return "Hello, <name> how are you doing today?".replace("<name>", name);
 }
+
+//sol2
+function greet(name){
+  return `Hello, ${name} how are you doing today?`;
+}
