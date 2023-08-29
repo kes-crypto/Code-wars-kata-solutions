b96ffd9d7903a32ce5460a8867ddde5d7726b9d5
Dennis Kestone <denniskestone@gmail.com>
2023-08-29 18:02:19 +0300

    Update order.js

--- order.js
+++ order.js
@@ -21,3 +21,17 @@ const order = words => {
   });
   return result.join(' ');
 }
+
+//sol 2
+function order(words){
+  var array = words.split(' ');
+  var sortedArray = [];
+  for(i = 0; i <= array.length; i++) {
+    for(j = 0; j < array.length; j++) {
+      if(array[j].indexOf(i) >= 0) {
+        sortedArray.push(array[j]);
+      }
+    }
+  }
+  return sortedArray.join(' ');
+}
