08f13b8f452a6dc805b7a508462d17c3cc557798
Dennis Kestone <denniskestone@gmail.com>
2023-08-31 17:21:46 +0300

    Update needleHaystack.js

--- needleHaystack.js
+++ needleHaystack.js
@@ -1,1 +1,8 @@
-
+//my soln
+function findNeedle(haystack) {
+  const needle = haystack.indexOf("needle") 
+  return "found the needle at position " + needle
+}
+function findNeedle(haystack) {
+  return `found the needle at position ${haystack.indexOf('needle')}`;
+}
