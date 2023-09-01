9e39f73b6524bf2feb6563d7c9b21cc88162cad1
Dennis Kestone <denniskestone@gmail.com>
2023-09-01 18:37:23 +0300

    Update reverse.js

--- reverse.js
+++ reverse.js
@@ -2,3 +2,10 @@
 function solution(str){
   return str.split('').reverse().join('');  
 }
+//sol1
+function solution(s){
+  var o = '';
+  for (var i = s.length - 1; i >= 0; i--)
+    o += s[i];
+  return o;
+}
