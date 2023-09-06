74598e7f0e297940a113ba46e4e05f456e313874
Dennis Kestone <denniskestone@gmail.com>
2023-09-06 13:39:13 +0300

    Update sumOfOdd.js

--- sumOfOdd.js
+++ sumOfOdd.js
@@ -22,3 +22,15 @@ function rowSumOddNumbers(n) {
 }
 //sol3
 let rowSumOddNumbers = n => n ** 3;
+//sol4
+function rowSumOddNumbers(n) {
+	var start = n * n - n + 1;
+  var result = 0;  
+  
+  for(i = 0; i < n; i++)
+  {
+    result =  result + (start + (i*2));
+  }
+    
+   return result;
+}
