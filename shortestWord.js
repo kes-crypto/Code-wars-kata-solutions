1a6786b98e5ccb0c5d1a7a51b39e7931fc48dc7b
Dennis Kestone <denniskestone@gmail.com>
2023-09-07 09:12:14 +0300

    Update shortestWord.js

--- shortestWord.js
+++ shortestWord.js
@@ -1,1 +1,18 @@
+/*
+Simple, given a string of words, return the length of the shortest word(s).
 
+String will never be empty and you do not need to account for different data types.
+*/
+function findShort(s){
+  const word = s.split(' ')
+  const result = word.map(a => a.length);
+  return Math.min(...result);
+}
+
+//compare to
+function findShort(s){
+  //var word = s.split(' ')
+  var result = parseInt(word.map(a => a.length))
+  return result
+  return Math.min(result)
+}
