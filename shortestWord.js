1e47bfa165161443210276da5a3cb1589b18b345
Dennis Kestone <denniskestone@gmail.com>
2023-09-07 09:13:27 +0300

    Update shortestWord.js

--- shortestWord.js
+++ shortestWord.js
@@ -20,3 +20,14 @@ function findShort(s){
 function findShort(s){
     return Math.min(...s.split(" ").map (s => s.length));
 }
+//sol3
+function findShort(s){
+	var arr = s.split(' ');
+	var smallest = arr[0];
+	for (var i = 0; i < arr.length; i++) {
+		if(arr[i].length < smallest.length){
+			smallest = arr[i];
+		}
+	}
+	return smallest.length;
+}
