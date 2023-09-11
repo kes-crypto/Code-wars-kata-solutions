1ba5368bcba94e41285f5d752bef227b3a3fac5b
Dennis Kestone <denniskestone@gmail.com>
2023-09-11 17:54:05 +0300

    Update highAndLow.js

--- highAndLow.js
+++ highAndLow.js
@@ -10,3 +10,11 @@ All numbers are valid Int32, no need to validate them.
 There will always be at least one number in the input string.
 Output string must be two numbers separated by a single space, and highest number is first.
 */
+//try
+function highAndLow(numbers) {
+  var number = Array.from(numbers)
+  var sorted = number.sort()
+  var high = sorted[-1]
+  var low = sorted[0]
+  return high low
+  }
