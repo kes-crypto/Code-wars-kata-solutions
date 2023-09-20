d6cd089979f131bf11290a0ee94618531bec8c44
Dennis Kestone <denniskestone@gmail.com>
2023-09-20 12:39:13 +0300

    Update abbreviateWord.js

--- abbreviateWord.js
+++ abbreviateWord.js
@@ -1,1 +1,11 @@
+function abbrevName(name){
 
+   'use strict'
+    
+    return name
+        .replace(/\b(\w)\w+/g, '$1.')
+        .replace(/\s/g, '') 
+        .replace(/\.$/, '')
+        .toUpperCase();
+
+}
