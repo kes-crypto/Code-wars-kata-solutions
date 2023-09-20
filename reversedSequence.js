6d15de78e5295d36e3964d9cf46467ef78c93cfd
Dennis Kestone <denniskestone@gmail.com>
2023-09-20 14:14:56 +0300

    Update reversedSequence.js

--- reversedSequence.js
+++ reversedSequence.js
@@ -3,3 +3,12 @@ Build a function that returns an array of integers from n to 1 where n>0.
 
 Example : n=5 --> [5,4,3,2,1]
 */
+
+function reverseSeq(N) {
+    let newArr = [];
+    for (let i = 1; i <= N; i++) {
+        newArr.push(i);
+      
+    }
+    return newArr.reverse();
+}
