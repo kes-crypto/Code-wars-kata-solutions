e403d411c42e9b902850d61e53fde0c12522a6c8
Dennis Kestone <denniskestone@gmail.com>
2023-09-20 12:41:20 +0300

    Update abbreviateWord.js

--- abbreviateWord.js
+++ abbreviateWord.js
@@ -9,3 +9,11 @@ function abbrevName(name){
         .toUpperCase();
 
 }
+//sol1
+const answer = [name[0]]
+    for(let i = 0; i < name.length; i++) {
+        if(name[i] === " ") {
+            answer.push(name[i + 1]);
+        }
+    }
+    return answer.map(el => el.toUpperCase()).join(".")
