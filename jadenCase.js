3847516fd1e47ffb9d32ded1a95b776f5f4d7236
Dennis Kestone <denniskestone@gmail.com>
2023-09-20 12:44:24 +0300

    Update jadenCase.js

--- jadenCase.js
+++ jadenCase.js
@@ -7,3 +7,12 @@ String.prototype.toJadenCase = function () {
 String.prototype.toJadenCase = function() {
   return this.split(' ').map(item => item[0].toUpperCase() + item.slice(1)).join(' ')
 };
+//sol2
+String.prototype.toJadenCase = function () {
+
+  function capitalizeFirstLetter(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  
+  return this.split(' ').map(capitalizeFirstLetter).join(' ');
+};
