d600adbea1a3cf720444a81d062dc668a4de4a80
Dennis Kestone <denniskestone@gmail.com>
2023-09-20 14:28:07 +0300

    Update reversedSequence.js

--- reversedSequence.js
+++ reversedSequence.js
@@ -19,3 +19,13 @@ let arr = [];
     arr.push(i);
     } return arr;
 };
+//sol2
+const reverseSeq = n => {
+  let answer = [];          //intialize an array
+  
+  for (let i=n; i>0; i--){  //loop down from n to 1
+    answer.push(i);         //push each i to the answer array
+  }
+  
+  return answer;            //return answer;
+};
