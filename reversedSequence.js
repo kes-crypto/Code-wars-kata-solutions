1f3766d6de89b0497f844513fef38b74556b1c2f
Dennis Kestone <denniskestone@gmail.com>
2023-09-20 14:26:50 +0300

    Update reversedSequence.js

--- reversedSequence.js
+++ reversedSequence.js
@@ -12,3 +12,10 @@ function reverseSeq(N) {
     }
     return newArr.reverse();
 }
+//sol1
+const reverseSeq = n => {
+let arr = [];
+  for (let i=n; i>0; i--) {
+    arr.push(i);
+    } return arr;
+};
