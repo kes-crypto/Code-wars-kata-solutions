3a8b2391cbddb52b3807d2c4c83a0469a825f9ae
Dennis Kestone <denniskestone@gmail.com>
2023-09-21 17:09:46 +0300

    Update domainName.js

--- domainName.js
+++ domainName.js
@@ -3,7 +3,7 @@ For example:
  url = "http://github.com/carbonfive/raygun" -> domain name = "github"
  url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
  url = "https://www.cnet.com"                -> domain name = cnet"
-
+replace function
 Solution:
 */
 function domainName(url){
