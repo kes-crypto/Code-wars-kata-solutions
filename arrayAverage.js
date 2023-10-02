2ba59d7294d75b0dd1cb99631661508f322111e3
Dennis Kestone <denniskestone@gmail.com>
2023-10-02 16:10:46 +0300

    Update arrayAverage.js

--- arrayAverage.js
+++ arrayAverage.js
@@ -3,3 +3,17 @@ Write a function which calculates the average of the numbers in a given list.
 
 Note: Empty arrays should return 0.
 */
+function findAverage(array) {
+  // create a variable for the sum and initialize it
+  let sum = 0;
+  
+  // iterate over each item in the array
+for (let i = 0; i < array.length; i++ ) {
+  sum += array[i];
+}
+if (array.length>0){
+  return sum/array.length
+}
+  else
+  return 0;
+}
