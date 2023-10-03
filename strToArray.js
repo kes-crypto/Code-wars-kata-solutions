72892235b746d49dd8d0de285eb62bb80137c0bf
Dennis Kestone <denniskestone@gmail.com>
2023-10-03 09:52:36 +0300

    Update strToArray.js

--- strToArray.js
+++ strToArray.js
@@ -6,3 +6,28 @@ Examples (Input ==> Output):
 
 "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
 */
+function stringToArray(string){
+
+	var arr = string.split(' ')
+ return arr
+
+}
+
+const stringToArray = string => string.split(' ');
+
+function stringToArray(string){
+    var result = [];
+    var traverse = function foo(string){
+        for(var i=0;i<string.length;i++){
+            var chr = string.charAt(i);
+            if(chr === " "){
+                result.push(string.slice(0,i));
+                foo(string.slice(i+1));
+                return;
+            }
+        }
+        result.push(string);
+    };
+    traverse(string);
+    return result;
+}
