19b9dbd64679d9643ceb93da9c56a4070d7f3f0a
Dennis Kestone <denniskestone@gmail.com>
2023-10-04 18:16:00 +0300

    Update smiley.js

--- smiley.js
+++ smiley.js
@@ -18,3 +18,7 @@ countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;
 Note
 In case of an empty array return 0. You will not be tested with invalid input (input will always be an array). Order of the face (eyes, nose, mouth) elements will always be the same.
 */
+//return the total number of smiling faces in the array
+function countSmileys(arr) {
+  return 0;
+}
