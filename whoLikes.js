f3c624f9e46e0389773c5cdefdfbd043ef444415
Dennis Kestone <denniskestone@gmail.com>
2023-10-06 13:04:07 +0300

    Update whoLikes.js

--- whoLikes.js
+++ whoLikes.js
@@ -53,3 +53,11 @@ function likes(names) {
     4: `${names[0]}, ${names[1]} and ${names.length - 2} others like this`, 
   }[Math.min(4, names.length)]
 }
+//sol3
+function likes(names) {
+  if(names.length === 0) return "no one likes this";
+  if(names.length === 1) return names[0] + " likes this";
+  if(names.length === 2) return names[0] + " and " + names[1] + " like this";
+  if(names.length === 3) return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+  return names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this";
+}
