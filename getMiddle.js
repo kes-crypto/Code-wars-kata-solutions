60795b4f70fa64a6b0424d0e7bbbf4de26f54aae
Dennis Kestone <denniskestone@gmail.com>
2023-10-07 10:20:35 +0300

    Update getMiddle.js

--- getMiddle.js
+++ getMiddle.js
@@ -33,3 +33,8 @@ The middle character(s) of the word represented as a string.
 
         return str.substring(position, position + length)
     }
+//sol1
+function getMiddle(s)
+{
+  return s.substr(Math.ceil(s.length / 2 - 1), s.length % 2 === 0 ? 2 : 1);
+}
