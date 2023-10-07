4dab9f1f46b956f683afb6391b7eb3822586fb34
Dennis Kestone <denniskestone@gmail.com>
2023-10-07 09:59:59 +0300

    Update isTheSame.js

--- isTheSame.js
+++ isTheSame.js
@@ -80,3 +80,10 @@ function comp(array1, array2) {
 function comp(a, b) {
   return !!a && !!b && a.map(x => x*x).sort().join() == b.sort().join();
 }
+//sol3
+function comp(array1, array2) {
+  if (array1 == null || array2 == null) return false;
+  array1.sort((a, b) => a - b);
+  array2.sort((a, b) => a - b);
+  return array1.every((item, index) => item * item == array2[index]);
+}
