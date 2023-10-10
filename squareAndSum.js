c2862c8370c4e5ee88e86b0cf0d2ea7a4eb37bb2
Dennis Kestone <denniskestone@gmail.com>
2023-10-10 12:57:35 +0300

    Update squareAndSum.js

--- squareAndSum.js
+++ squareAndSum.js
@@ -45,3 +45,11 @@ function squareSum(numbers){
   });
   return sum;
 }
+//sol3
+function squareSum(numbers){
+  var result = 0;
+  for (var i = 0; i < numbers.length; i++) {
+    result += (numbers[i] * numbers[i]);
+  }
+  return result;
+}
