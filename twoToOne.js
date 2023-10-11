b535ae22dfbce2e7cdfa0aa1ac048a5bdbcfa21f
Dennis Kestone <denniskestone@gmail.com>
2023-10-11 14:11:18 +0300

    Update twoToOne.js

--- twoToOne.js
+++ twoToOne.js
@@ -9,3 +9,17 @@ longest(a, b) -> "abcdefklmopqwxy"
 a = "abcdefghijklmnopqrstuvwxyz"
 longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
 */
+
+const longest = (str1 = '', str2 = '') => {
+   const combined = str1.concat(str2);
+   const lower = combined.toLowerCase();
+   const split =lower.split('');
+   const sorted = split.sort();
+   const res = [];
+   for(const el of sorted){
+      if(!res.includes(el)){
+         res.push(el)
+      }
+   }
+   return (res.join(''));
+};
