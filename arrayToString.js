cd9117ea6d5737dac29e7c069819a77834ad5884
Dennis Kestone <denniskestone@gmail.com>
2023-10-14 10:50:36 +0300

    Update arrayToString.js

--- arrayToString.js
+++ arrayToString.js
@@ -12,3 +12,15 @@ function smash (words) {
 smash = function (words) {
   return words.join(" ");
 };
+//sol2
+function smash (words) {
+    "use strict";
+    var smashed = '';
+    for(var i = 0; i<words.length; i++) {
+      smashed += words[i];
+      if(i!=words.length-1) {
+        smashed += ' ';
+      }
+    }
+    return smashed;
+};
