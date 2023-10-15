d117f8896dcca7d8f3c2d9f36ca0e816a99b84a8
Dennis Kestone <denniskestone@gmail.com>
2023-10-16 00:01:40 +0300

    Update sumWithoutHighestAndLowest.js

--- sumWithoutHighestAndLowest.js
+++ sumWithoutHighestAndLowest.js
@@ -12,3 +12,17 @@ Example
 Input validation
 If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
 */
+function sumArray(array) {
+  if (array == null) {
+    return 0;
+  } else if (array.length < 2) {
+    return 0;
+  } else {
+    array = array.sort(function(a,b) {return a - b;});
+    var total = 0;
+    for (var i = 1; i < array.length - 1; i++) {
+      total += array[i];
+    }
+    return total;
+  }
+}
