257dbd84d283ed0082f10e52097ad301da7a9a63
Dennis Kestone <denniskestone@gmail.com>
2023-10-18 10:30:00 +0300

    Update firstNonConsecutive.js

--- firstNonConsecutive.js
+++ firstNonConsecutive.js
@@ -65,3 +65,10 @@ function firstNonConsecutive (arr) {
   }
   return null; //default return
 }
+//sol5
+function firstNonConsecutive (arr) {
+  return arr.reduce((acc, n, i, src) => {
+    if(n !== src[i-1] + 1 && !acc && i !=0) return n;
+    return acc;
+  }, null);
+}
