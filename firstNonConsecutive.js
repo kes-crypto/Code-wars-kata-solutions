6499d0845c866f9ee55bd67bfad3a68f8bb0829f
Dennis Kestone <denniskestone@gmail.com>
2023-10-18 10:29:14 +0300

    Update firstNonConsecutive.js

--- firstNonConsecutive.js
+++ firstNonConsecutive.js
@@ -33,3 +33,23 @@ function firstNonConsecutive (arr) {
   }
   return null
 }
+//sol3
+function firstNonConsecutive (arr) {
+  // initial value 
+  // 1 2 3 4
+  let previousNum = arr[0]
+  // store first non consec
+  let first;
+  // if prevNum is not equal to next num in arr
+  // 2 3 4 6
+  for(let i = 1; i < arr.length; i++) {
+    if(previousNum + 1 !== arr[i]) {
+      // the non consec number gets stored here and end program or else it will look at next num in arr
+      first = arr[i]
+      break;
+    }
+    previousNum++
+  }
+  // return first non consec number not equal to undefined or first is null
+  return first !== undefined ? first : null
+}
