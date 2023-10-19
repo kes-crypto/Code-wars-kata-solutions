a5166c6ca2409c00eadb5e0242e0a2d7c4b32738
Dennis Kestone <denniskestone@gmail.com>
2023-10-19 12:45:43 +0300

    Update bindingNoToStr.js

--- bindingNoToStr.js
+++ bindingNoToStr.js
@@ -31,3 +31,12 @@ var newArr = [];
   }
   return newArr;
 }
+//sol4
+var number=function(array){
+  if (array.length == 0) return [];
+  
+  for (let i=0; i<array.length; ++i)
+    array[i] = (i+1) + ': ' + array[i];
+    
+  return array;
+}
