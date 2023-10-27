095768a70ae61fe155b19e17728e26f1d1824107
Dennis Kestone <denniskestone@gmail.com>
2023-10-27 14:09:14 +0300

    Update directions.js

--- directions.js
+++ directions.js
@@ -52,3 +52,9 @@ function dirReduc(arr){
     }
     return result
 }
+//sol1
+function dirReduc(arr) {
+  var str = arr.join(''), pattern = /NORTHSOUTH|EASTWEST|SOUTHNORTH|WESTEAST/;
+  while (pattern.test(str)) str = str.replace(pattern,'');
+  return str.match(/(NORTH|SOUTH|EAST|WEST)/g)||[];
+}
