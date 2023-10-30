f5ed5a92c71e39c7cd2510fcbf8f135031cea91e
Dennis Kestone <denniskestone@gmail.com>
2023-10-30 16:52:31 +0300

    Update duplicateEncoder.js

--- duplicateEncoder.js
+++ duplicateEncoder.js
@@ -9,3 +9,22 @@ Examples
 Notes
 Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
 */
+function duplicateEncode(str){
+//create a var to hold string value that IGNORES case
+var word = str.toLowerCase();
+//create a var to hold the finished string to return after it's looped through 
+var unique = '';
+//loop through all letters in string
+  for (var i = 0; i < word.length; i++) {
+//check for any characters that repeat
+    if (word.lastIndexOf(word[i]) === word.indexOf(word[i])) {
+//for each character that never dupes, place (
+      unique += '(';
+    } else
+//for each character that IS a dupe, place )
+    unique += ')';
+  }
+//return the full string value where '(' are non duped and all')' are duped
+  return unique;
+
+}
