c186ae14a7c35961caef0b9a5d1376a1b047d365
Dennis Kestone <denniskestone@gmail.com>
2023-11-01 16:11:52 +0300

    Update frequentlyUsedWords.js

--- frequentlyUsedWords.js
+++ frequentlyUsedWords.js
@@ -48,3 +48,13 @@ function topThreeWords(text) {
 
   return frequency.sort(function (a,b) { return frequencies[b] -frequencies[a];}).slice(0, 3)
 }
+//sol1
+let topThreeWords = text => {
+    let dict = new Map();
+    text.replace(/[A-z']+(?=[ ]+|$)/g, match => {
+        let word = match.toLowerCase();
+        dict.set(word, dict.has(word) ? dict.get(word) + 1 : 1);
+    });
+    dict.delete("'");
+    return [...dict].sort((a, b) => b[1] - a[1]).map(a => a[0]).slice(0, 3);
+};
