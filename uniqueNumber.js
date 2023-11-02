92e08cb972b31dd30ef61bcd103b39e117c9bd82
Dennis Kestone <denniskestone@gmail.com>
2023-11-02 17:23:03 +0300

    Update uniqueNumber.js

--- uniqueNumber.js
+++ uniqueNumber.js
@@ -22,3 +22,12 @@ const findUniq = (list, sorted = [...list].sort()) =>
         item !== sorted?.[index + 1] 
         && item !== sorted?.[index - 1]
    );
+//sol2
+function findUniq(arr) {
+  arr = arr.sort()
+  if (arr[0] === arr[1]){
+    return arr[arr.length -1]
+  } else {
+    return arr[0]
+  } 
+}
