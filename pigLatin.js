798fb1eac23a00be0075f689ca7f13a207330e3d
Dennis Kestone <denniskestone@gmail.com>
2023-11-03 17:31:20 +0300

    Update pigLatin.js

--- pigLatin.js
+++ pigLatin.js
@@ -26,3 +26,11 @@ function pigIt(str){
         return val;
     }).join(' ');
 }
+//sol2
+function pigIt(str) {
+  var arrayWord = str.split(' ');
+  return arrayWord.map(function(word) {
+    var firstLetter = word.charAt(0);
+    return word.slice(1) + firstLetter + 'ay';
+  }).join(' ');
+}
