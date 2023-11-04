1a80eef7fcb097d5edc75ee5a46528d6bacd56ae
Dennis Kestone <denniskestone@gmail.com>
2023-11-04 13:46:47 +0300

    Update bigNumber.js

--- bigNumber.js
+++ bigNumber.js
@@ -42,4 +42,11 @@ function narcissistic(value) {
        return false;
      }
 }
-
+//sol1
+function narcissistic(value) {
+  return value.toString()
+              .split('')
+              .map( (x,i,arr) => x ** arr.length)
+              .reduce( (a,b)=> +a + +b) 
+               === value
+}
