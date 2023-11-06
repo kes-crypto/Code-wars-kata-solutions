75e00f8c865718bc3ffb6ba71166a32bc4e72d4e
Dennis Kestone <denniskestone@gmail.com>
2023-11-06 17:17:26 +0300

    Update phoneNumber.js

--- phoneNumber.js
+++ phoneNumber.js
@@ -38,3 +38,6 @@ function createPhoneNumber(numbers){
   return '('+n[0]+n[1]+n[2]+') '+n[3]+n[4]+n[5]+'-'+n[6]+n[7]+n[8]+n[9];
 }
 //sol3
+function createPhoneNumber(numbers){
+  return '(' + numbers[0] + numbers[1] + numbers[2] + ') ' + numbers[3] + numbers[4] + numbers[5] + '-' + numbers[6] + numbers[7] + numbers[8] + numbers[9];
+}
