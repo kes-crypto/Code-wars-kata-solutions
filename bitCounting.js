709b2b40d8fee5469bf80067de0bda04c2e628d9
Dennis Kestone <denniskestone@gmail.com>
2023-11-08 08:33:49 +0300

    Update bitCounting.js

--- bitCounting.js
+++ bitCounting.js
@@ -3,3 +3,12 @@ Write a function that takes an integer as input, and returns the number of bits 
 
 Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
 */
+var countBits = function(n) {
+   // make an array with the bit result
+   const base = (n).toString(2).split('');
+   
+   // make a sum with the array and make the index a Number
+   const result = base.reduce((sum, num) => sum + Number(num), 0);
+   
+   return result;
+};
