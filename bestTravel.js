dcf4e7e3aded4b75aa7576ec858d229eac33e466
Dennis Kestone <denniskestone@gmail.com>
2023-11-11 08:57:36 +0300

    Update bestTravel.js

--- bestTravel.js
+++ bestTravel.js
@@ -62,3 +62,19 @@ function chooseBestSum(t, k, ls) {
 
   return combinationUtil(t, k, ls, store, 0, 0);
 }
+//sol2
+function chooseBestSum(t, k, ls){
+    if (ls.length < k) return null
+    const results = [[]]
+    for (const value of ls) {
+        const copy = [...results]
+        for (const prefix of copy) {
+            results.push(prefix.concat(value))
+        }
+    }
+    return results
+        .filter((a => a.length && a.length == k ))
+        .map(item => item.reduce((a,b) => a + b), 0)
+        .filter(item => item <= t)
+        .sort((a,b) => b-a)[0] || null
+}
