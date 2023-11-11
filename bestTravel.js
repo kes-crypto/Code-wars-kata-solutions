aa781452526f740f23b53550d07b7284802d2070
Dennis Kestone <denniskestone@gmail.com>
2023-11-11 08:56:17 +0300

    Update bestTravel.js

--- bestTravel.js
+++ bestTravel.js
@@ -23,3 +23,23 @@ Notes:
 try not to modify the input list of distances ls
 in some languages this "list" is in fact a string (see the Sample Tests).
 */
+function chooseBestSum(t, k, ls) {
+   var biggestCount = 0;
+   var recurseTowns = function(townsSoFar, lastIndex) {
+   townsSoFar = townsSoFar || [];
+   //base case
+   if (townsSoFar.length === k) {
+      var sumDistance = townsSoFar.reduce((a,b)=>a+b);
+         if (sumDistance <= t && sumDistance > biggestCount) {
+            biggestCount = sumDistance;
+         }
+      return; //EJECT
+   }
+//recursive case
+   for (var i = lastIndex + 1 || 0; i < ls.length; i++) {
+      recurseTowns(townsSoFar.concat(ls[i]), i);
+   }
+}
+recurseTowns();
+return biggestCount || null;
+}
