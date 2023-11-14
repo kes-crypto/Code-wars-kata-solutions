a2a208497ed5915ed22ebbc4fbfd874c53849378
Dennis Kestone <denniskestone@gmail.com>
2023-11-14 17:09:14 +0300

    Update arraySum.js

--- arraySum.js
+++ arraySum.js
@@ -21,3 +21,19 @@ Advanced users may find this extremely easy and can easily write this in one lin
 
 
 */
+// Sum Numbers
+function sum (numbers) {
+    "use strict";
+  let sum = 0  
+  
+  if (numbers.length===0){
+      return 0
+    }
+  else if(numbers.length>0){
+    for (let i=0;i<numbers.length;i++){
+      sum += numbers[i]
+    }
+    return sum
+  } 
+    
+};
