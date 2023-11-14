e2c3b8f85339b1031c35d802d6c3a4a210f70c31
Dennis Kestone <denniskestone@gmail.com>
2023-11-14 17:41:59 +0300

    Update tower.js

--- tower.js
+++ tower.js
@@ -20,3 +20,18 @@ And a tower with 6 floors looks like this:
   "***********"
 ]
 */
+function towerBuilder(nFloors) {
+  var finalArray = [];
+  var numSpaces = 0;
+  // Start from the reverse.
+  for (i = nFloors; i > 0; i--) {
+    // At the end, the number of spaces are 0.
+    // Have one and less for the half of the stars.
+    // Add another set of spaces.
+    finalArray.push(" ".repeat(numSpaces) + "*".repeat(i) + "*".repeat(i-1)  + " ".repeat(numSpaces));
+    // Increment the space, while going to top.
+    numSpaces++;
+  }
+  // Reverse the array.
+  return finalArray.reverse();
+}
