0b4203265c72587fe8027346576f2bd149ce4bc6
Dennis Kestone <denniskestone@gmail.com>
2023-11-18 08:52:26 +0300

    Update equalSidesOfAnArray.js

--- equalSidesOfAnArray.js
+++ equalSidesOfAnArray.js
@@ -46,3 +46,15 @@ function findEvenIndex(arr)
   
   return -1;
 }
+//sol2
+function findEvenIndex(arr)
+{
+  let left = 0;
+  let right = arr.reduce((s,n) => s + n, 0);
+  for (let i = 0; i < arr.length; i++) {
+    right -= arr[i];
+    if (left === right) return i;
+    left += arr[i];
+  }
+  return -1;
+}
