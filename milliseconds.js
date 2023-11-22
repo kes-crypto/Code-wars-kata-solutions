369e4d8773b02b6259b0f33f406693d57015fc86
Dennis Kestone <denniskestone@gmail.com>
2023-11-22 14:47:51 +0300

    Update milliseconds.js

--- milliseconds.js
+++ milliseconds.js
@@ -22,3 +22,10 @@ function past(h, m, s){
 function past(h, m, s){
   return ((h*3600)+(m*60)+s)*1000;
 }
+//sol3
+function past(h, m, s){
+  const setTime = new Date().setHours(h, m, s);
+  const midnight = new Date().setHours(0, 0, 0);
+  
+  return Math.round(setTime - midnight);
+}
