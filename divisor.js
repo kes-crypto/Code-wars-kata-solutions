8edecf8a9786ddeab8d8eab9753b43c412645671
Dennis Kestone <denniskestone@gmail.com>
2023-11-22 09:23:28 +0300

    Update divisor.js

--- divisor.js
+++ divisor.js
@@ -14,3 +14,12 @@ function divisors(integer) {
   let res = r.length !== 0 ? r : `${integer} is prime`
   return res
 }
+//sol1
+function divisors(integer) {
+    var divisors = [];
+    for (var i = 2; i < integer - 1; i++){
+        if (integer % i === 0) divisors.push(i);
+    }
+    if (divisors.length === 0) return '' + integer + ' is prime';
+    return divisors;
+}
