a6fdd88ef1e24ca1dc7fdd30bb0c8f3420bf1b40
Dennis Kestone <denniskestone@gmail.com>
2023-11-23 10:48:18 +0300

    Update smiley.js

--- smiley.js
+++ smiley.js
@@ -29,3 +29,35 @@ function smileyHasValidMouth(s) {
   }
   return;
 }
+//sol1
+function countSmileys(smileys) {
+  let firstStep = smileyHasValidEye;
+  return smileys.filter(smiley => {
+    let state = firstStep;
+    for (let s of [...smiley]) {
+      state = state(s);
+      if (typeof state !== 'function') return state;
+    }
+  }).length;
+}
+
+function smileyHasValidEye(s) {
+  if (s === ':' || s === ';') {
+    return smileyHasValidNose;
+  }
+  return smileyHasValidEye;
+}
+
+function smileyHasValidNose(s) {
+  if (s === '-' || s === '~') {
+    return smileyHasValidMouth;
+  }
+  return smileyHasValidMouth(s);
+}
+
+function smileyHasValidMouth(s) {
+  if (s === ')' || s === 'D') {
+    return true;
+  }
+  return false;
+}
