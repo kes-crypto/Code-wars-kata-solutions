a3f04ef678f9a395cfde7172b3639e6fb843d262
Dennis Kestone <denniskestone@gmail.com>
2023-11-24 10:34:44 +0300

    Update regexPin.js

--- regexPin.js
+++ regexPin.js
@@ -2,3 +2,17 @@ function validatePIN(pin) {
 var isNumber = /^\d+$/.test(pin) && (pin.length == 4 || pin.length == 6)
 return isNumber
 }
+//sol2
+function validatePIN (pin) {
+  
+  var pinlen = pin.length;
+  var isCorrectLength = (pinlen == 4 || pinlen == 6);
+  var hasOnlyNumbers = pin.match(/^\d+$/);
+    
+  if(isCorrectLength && hasOnlyNumbers){
+    return true;
+  }
+  
+  return false;
+
+}
