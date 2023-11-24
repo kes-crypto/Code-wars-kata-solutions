7d6be4871d6b3f91867dd717a0dbe1f4f5fcd4df
Dennis Kestone <denniskestone@gmail.com>
2023-11-24 10:35:16 +0300

    Update regexPin.js

--- regexPin.js
+++ regexPin.js
@@ -16,3 +16,14 @@ function validatePIN (pin) {
   return false;
 
 }
+//sol2
+function validatePIN (pin) {
+  //return true or false
+	var n = pin.length;
+  if( n != 4 && n != 6)
+  		return false;
+  for (var i in pin)
+  		if (pin[i] > '9' || pin[i] < '0')
+      		return false;
+  return true;
+}
