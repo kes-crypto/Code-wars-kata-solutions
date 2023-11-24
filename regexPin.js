804ff16c566880c92f55053c1e4578c0c07d4ea6
Dennis Kestone <denniskestone@gmail.com>
2023-11-24 10:35:48 +0300

    Update regexPin.js

--- regexPin.js
+++ regexPin.js
@@ -27,3 +27,18 @@ function validatePIN (pin) {
       		return false;
   return true;
 }
+//sol3
+/* for my future referencing,
+
+  ^ = start of a line, 
+  \d = [0-9], {4} specifies 4 digits, 
+  {6} specifies 6 digits, $ - end of a line
+  
+  first alternative ^\d{4}$ - equivalent to [0-9], matches exactly 4 digits.
+  second alternative ^\d{6}$ - "", matches previous token exactly 6 digits.
+  
+   use RegEx .test to search for these characters stored.
+   
+*/ 
+
+const validatePIN = pin => /^\d{4}$|^\d{6}$/.test(pin);
