1b8b8aaa083e41b2c4fbdff7cc600ac66da26a39
Dennis Kestone <denniskestone@gmail.com>
2023-11-25 14:08:55 +0300

    Update triangle.js

--- triangle.js
+++ triangle.js
@@ -1,3 +1,8 @@
 function isTriangle(a, b, c) {
   return (a + b > c) && (a + c > b) && (b + c > a);
 }
+//sol1
+function isTriangle(a,b,c) {
+  var sides = [a, b, c].sort();
+  return (sides[0] + sides[1] > sides[2]);
+}
