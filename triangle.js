98377686496bed90e0b0ec54323cdc9e48c6e971
Dennis Kestone <denniskestone@gmail.com>
2023-11-25 14:10:02 +0300

    Update triangle.js

--- triangle.js
+++ triangle.js
@@ -12,3 +12,8 @@ function isTriangle(a,b,c)
     return Math.min(a+b,a+c,b+c) > Math.max(a,b,c) ? true:false
   
 }
+//sol3
+function isTriangle(a,b,c){
+   let sides = [a, b, c].sort((a,b) => a - b)
+   return (sides[0] + sides[1]) > sides[2]
+}
