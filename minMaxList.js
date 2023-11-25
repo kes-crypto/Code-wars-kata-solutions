dd413804a2120b5f1b8630d94d5a4989b57fdb8d
Dennis Kestone <denniskestone@gmail.com>
2023-11-25 14:31:20 +0300

    Update minMaxList.js

--- minMaxList.js
+++ minMaxList.js
@@ -7,3 +7,13 @@ var max = function(list){
     
     return Math.max(...list)
 }
+//sol1
+var min = function(list){
+    list.sort((a, b) => (a - b));
+    return list[0];
+}
+
+var max = function(list){
+    list.sort((a, b) => (b - a));
+    return list[0];
+}
