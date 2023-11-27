b9dd8f3b1e17c427007b98bbfe6831f7b84ab771
Dennis Kestone <denniskestone@gmail.com>
2023-11-27 09:31:59 +0300

    Update spinWords.js

--- spinWords.js
+++ spinWords.js
@@ -1,1 +1,12 @@
-
+const spinWords = (string) => {
+  const re = / /g;
+  const stringArray = string.split(re)
+  
+  return (
+  [stringArray.map(element => {
+    return (element.length >= 5 ? 
+            [...element].reverse().join(" ").replace(re, "") 
+            : element)
+  })]
+  )[0].join(" ")
+}
