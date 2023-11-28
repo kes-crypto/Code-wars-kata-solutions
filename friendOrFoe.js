c09cad3712355920eb83ac64678be0e182e3e8c0
Dennis Kestone <denniskestone@gmail.com>
2023-11-28 10:55:33 +0300

    Update friendOrFoe.js

--- friendOrFoe.js
+++ friendOrFoe.js
@@ -1,3 +1,15 @@
 function friend(friends){
   return friends.filter(el => el.length === 4);
 }
+//sol1
+function friend(friends){
+  //your code here
+  var realFriends = [];
+  for(i=0; i<friends.length; i++){
+    if(friends[i].length == 4 && isNaN(friends[i])){
+      realFriends.push(friends[i]);
+    }
+  }
+  
+  return realFriends
+}
