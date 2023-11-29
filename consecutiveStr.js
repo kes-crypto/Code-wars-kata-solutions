2e2eec0a4e418a869728dd5f53476f4054e78259
Dennis Kestone <denniskestone@gmail.com>
2023-11-29 15:05:45 +0300

    Update consecutiveStr.js

--- consecutiveStr.js
+++ consecutiveStr.js
@@ -28,3 +28,18 @@ function longestConsec(strarr, k) {
       : long;
   }, '');
 }
+//sol2
+function longestConsec(strarr, k) {
+    if (strarr.length === 0 || k > strarr.length || k <= 0) return "";
+    let result = '';
+    for (let i = 0; i <= strarr.length - k; i++) {
+      let str = '';
+      for (let j = i; j < i + k; j++) {
+        str += strarr[j];
+      }
+      if (result.length < str.length) {
+        result = str;
+      }
+    }
+    return result;
+}
