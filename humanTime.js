f42efd2a05e15a52c7d02e9b59557464b3d83eeb
Dennis Kestone <denniskestone@gmail.com>
2023-12-04 17:11:31 +0300

    Update humanTime.js

--- humanTime.js
+++ humanTime.js
@@ -35,3 +35,15 @@ function formatDate(n){
   var number = Number.parseInt(n)
   return number > 9? number : '0'+number;
 }
+sol2
+function humanReadable(seconds) {
+var hours = parseInt( seconds / 3600 ) ;
+var minutes = parseInt( seconds / 60 ) % 60;
+var seconds = seconds % 60;
+
+var pad = function(val){
+  return val < 10 ?"0"+val:val;
+}
+
+return pad(hours) + ":" +pad(minutes) + ":" + pad(seconds);
+}
