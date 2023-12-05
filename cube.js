55dc18c94005a84f6d6c867c51480f8480a20c4c
Dennis Kestone <denniskestone@gmail.com>
2023-12-05 13:51:25 +0300

    Update cube.js

--- cube.js
+++ cube.js
@@ -26,3 +26,12 @@ function findNb(m) {
   return m ? -1 : n
 }
 //sol2
+function findNb(m) {
+  let n = 0;
+  let sum = 0;
+  while (sum < m) {
+    n++;
+    sum += Math.pow(n, 3);
+  }
+  return sum === m ? n : -1;
+}
