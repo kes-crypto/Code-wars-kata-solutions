cd11e479a9b009ee72f08c5c3f9d32c9a331ac96
Dennis Kestone <denniskestone@gmail.com>
2023-12-15 15:46:14 +0300

    Update countingDuplicates.js

--- countingDuplicates.js
+++ countingDuplicates.js
@@ -15,3 +15,9 @@ function duplicateCount(text) {
     }
     return counter;
 }
+//sol1
+function duplicateCount(text){
+  return text.toLowerCase().split('').filter(function(val, i, arr){
+    return arr.indexOf(val) !== i && arr.lastIndexOf(val) === i;
+  }).length;
+}
