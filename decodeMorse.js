bc9ca55709e560b84f81134040a8e658cecd4a07
Dennis Kestone <denniskestone@gmail.com>
2023-12-18 15:39:18 +0300

    Update decodeMorse.js

--- decodeMorse.js
+++ decodeMorse.js
@@ -1,1 +1,53 @@
+onst decodeLetter = letter => {
+  return MORSE_CODE[letter];
+}
 
+const decodeWord = word => {
+  return word.split(' ').map(decodeLetter).join('');
+}
+
+const decodeMorse = morseCode => {
+  return morseCode.trim().split('   ').map(decodeWord).join(' ');
+}
+
+
+// helper - this is preloaded Morse code:
+
+const MORSE_CODE = {
+  '.-': 'A',
+  '-…': 'B',
+  '-.-.': 'C',
+  '-..': 'D',
+  '.': 'E',
+  '..-.': 'F',
+  '--.': 'G',
+  '….': 'H',
+  '..': 'I',
+  '.---': 'J',
+  '-.-': 'K',
+  '.-..': 'L',
+  '--': 'M',
+  '-.': 'N',
+  '---': 'O',
+  '.--.': 'P',
+  '--.-': 'Q',
+  '.-.': 'R',
+  '…': 'S',
+  '-': 'T',
+  '..-': 'U',
+  '…-': 'V',
+  '.--': 'W',
+  '-..-': 'X',
+  '-.--': 'Y',
+  '--..': 'Z',
+  '-----': '0',
+  '.----': '1',
+  '..---': '2',
+  '…--': '3',
+  '….-': '4',
+  '…..': '5',
+  '-….': '6',
+  '--…': '7',
+  '---..': '8',
+  '----.': '9',
+}
