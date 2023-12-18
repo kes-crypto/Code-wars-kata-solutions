29748528e17f1c7601d3a7e8cb16345b2a07d886
Dennis Kestone <denniskestone@gmail.com>
2023-12-18 17:08:52 +0300

    Update decodeMorse.js

--- decodeMorse.js
+++ decodeMorse.js
@@ -71,3 +71,13 @@ decodeMorse = function(morseCode){
   }
   return result.join('');
 }
+//sol2
+decodeMorse = function(morseCode){
+  function decodeMorseLetter(letter) {
+    return MORSE_CODE[letter];
+  }
+  function decodeMorseWord(word) {
+    return word.split(' ').map(decodeMorseLetter).join('');
+  }
+  return morseCode.trim().split('   ').map(decodeMorseWord).join(' ');
+}
