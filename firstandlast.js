3ab1774f7603f55b5417057e5c72ce91edd5b2af
Dennis Kestone <denniskestone@gmail.com>
2023-12-19 18:06:30 +0300

    Create firstandlast.js

---
+++ firstandlast.js
@@ -0,0 +1,14 @@
+function removeChar(str){
+ //You got this!
+  let newStr = ""
+  for(i = 0 ; i < str.length ; i++){
+    if(i == 0){
+      newStr += ""
+    }else if(i > 0 && i < (str.length-1)){
+      newStr += str[i]
+    }else if (i == str.length){
+      newStr += ""
+    }
+  }
+  return newStr
+};
