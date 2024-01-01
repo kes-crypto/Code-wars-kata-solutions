4bbd8759ebb966de8d49167fe5d048424ce02605
Dennis Kestone <denniskestone@gmail.com>
2024-01-01 22:08:41 +0300

    Update bouncingballs.js

--- bouncingballs.js
+++ bouncingballs.js
@@ -4,3 +4,19 @@ function bouncingBall(h,  bounce,  window) {
   if (bounce > 0 && bounce < 1) while (h > window) rebounds+=2, h *= bounce;
   return rebounds;
 }
+//sol1
+function bouncingBall(h, bounce, window) {
+  if (h <= 0 || bounce <= 0 || bounce >= 1 || window >= h) return -1;
+  
+  let seen = 0;
+  
+  while (h > window) {
+    seen++;
+    h *= bounce;
+    
+    if (h > window) {
+      seen++;
+    }
+  }
+  return seen;
+}
