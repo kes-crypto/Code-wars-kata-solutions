f00951506c0cb1536ddbacef8c014dd977418527
Dennis Kestone <denniskestone@gmail.com>
2024-01-31 16:59:17 +0300

    Update strToCamelCase.js

--- strToCamelCase.js
+++ strToCamelCase.js
@@ -9,3 +9,20 @@ function toCamelCase(str){
   }
   return arr.join('');
 }
+//sol1
+function toCamelCase(str){
+  var strArray;
+  if (str.indexOf('-') !== -1){ //if delineated by -
+    strArray = str.split('-');
+  } else {
+    strArray = str.split('_');  //if delineated by _
+  }
+  var camelCase = strArray[0]; //keeps first word value as is
+  for (var i=1, len=strArray.length; i < len; i++){
+    var capitalized = strArray[i].substr(0, 1).toUpperCase() + strArray[i].slice(1);
+    camelCase += capitalized;
+  }
+  return camelCase;
+  
+
+}
