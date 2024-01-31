99d8745cc6eae1334ed556e153f3a757db0b2035
Dennis Kestone <denniskestone@gmail.com>
2024-01-31 16:58:41 +0300

    Create strToCamelCase.js

---
+++ strToCamelCase.js
@@ -0,0 +1,11 @@
+function toCamelCase(str){ 
+  let arr = str.split('');
+  for(i = 0; i < arr.length; i++){
+    let letter = arr[i];
+    if(letter == '_' || letter == '-') {
+      arr[i + 1] = arr[i + 1].toUpperCase();
+      arr[i] = '';
+    } 
+  }
+  return arr.join('');
+}
