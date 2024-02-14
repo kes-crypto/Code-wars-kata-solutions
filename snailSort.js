b9f93ec18bd800886f1eb44415c69f9fe06e8a14
Dennis Kestone <denniskestone@gmail.com>
2024-02-14 15:35:57 +0300

    Update snailSort.js

--- snailSort.js
+++ snailSort.js
@@ -9,3 +9,20 @@ snail = function(array) {
   let inner = array.slice(1, -1).map(c => c.slice(1, -1));
   return [].concat(top, right, bottom, left, snail(inner));
 }
+//sol1
+snail = function(array) {
+  var result;
+  while (array.length) {
+    // Steal the first row.
+    result = (result ? result.concat(array.shift()) : array.shift());
+    // Steal the right items.
+    for (var i = 0; i < array.length; i++)
+      result.push(array[i].pop());
+    // Steal the bottom row.
+    result = result.concat((array.pop() || []).reverse());
+    // Steal the left items.
+    for (var i = array.length - 1; i >= 0; i--)
+      result.push(array[i].shift());
+  }
+  return result;
+}
