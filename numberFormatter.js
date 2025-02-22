8aaeb6ebb5664fc305747e1bf561aba8ec481e47
Dennis Kestone <denniskestone@gmail.com>
2025-02-22 13:45:56 +0300

    Create numberFormatter.js

---
+++ numberFormatter.js
@@ -0,0 +1,25 @@
+//sol 1
+function createPhoneNumber(numbers){
+  var format = "(xxx) xxx-xxxx";
+  
+  for(var i = 0; i < numbers.length; i++)
+  {
+    format = format.replace('x', numbers[i]);
+  }
+  
+  return format;
+}
+
+//sol 2
+function createPhoneNumber(numbers){
+  numbers = numbers.join('');
+  return '(' + numbers.substring(0, 3) + ') ' 
+      + numbers.substring(3, 6) 
+      + '-' 
+      + numbers.substring(6);
+}
+
+//sol 3
+function createPhoneNumber(numbers){
+  return numbers.join('').replace(/(...)(...)(.*)/, '($1) $2-$3');
+}
